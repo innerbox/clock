469ee5f074e88966ef2094e17d5d96c7390ea6fe
innerbox <difogid614@gmail.com>
2022-10-31 07:35:15 +0900

    moment.js 로 변경

--- js/clock.js
+++ js/clock.js
@@ -1,15 +1,16 @@
 const clock = document.querySelector("main");
 
 function showClock() {
-    const date = new Date();
     /*
+    const date = new Date();
     const hour = String(date.getHours()).padStart(2, "0");
     const minute = String(date.getMinutes()).padStart(2, "0");
     const second = String(date.getSeconds()).padStart(2, "0");
 
     clock.innerHTML = `&#128345;${hour}:${minute}:${second}`;
     */
-    clock.innerHTML = date.toLocaleString("ko-KR");
+    const date = moment();
+    clock.innerHTML = date.format("YYYY년 MM월 DD일 HH시 mm분 ss초 dddd");
 }
 
 showClock();
