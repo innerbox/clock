b22279db55e5320f2a41a911018edc73c46e8e18
innerbox <difogid614@gmail.com>
2022-10-31 06:28:06 +0900

    toLocaleString 로 변경

--- js/clock.js
+++ js/clock.js
@@ -2,11 +2,14 @@ const clock = document.querySelector("main");
 
 function showClock() {
     const date = new Date();
+    /*
     const hour = String(date.getHours()).padStart(2, "0");
     const minute = String(date.getMinutes()).padStart(2, "0");
     const second = String(date.getSeconds()).padStart(2, "0");
 
     clock.innerHTML = `&#128345;${hour}:${minute}:${second}`;
+    */
+    clock.innerHTML = date.toLocaleString("ko-KR");
 }
 
 showClock();
